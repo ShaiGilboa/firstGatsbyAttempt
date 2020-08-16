87e69c33660b27722ed5f271a795773b057eae48
Shai Gilboa <shizel@gmail.com>
2020-08-16 17:11:33 -0400

    part 3

--- app/src/pages/about.js
+++ app/src/pages/about.js
@@ -1,29 +1,22 @@
-// import React, {
-//   useEffect,
-//   useState,
-// } from 'react';
-
-// const About = () => {
-
-//   return (
-//     <div style={{ color: `teal` }}>
-//       <h1>About Gatsby</h1>
-//       <p>Such wow. Very React.</p>
-//   </div>
-//     );
-// }
-
-// export default About;
-
 import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
 import Header from "../components/header"
 
 export default function About() {
   return (
+    <Layout>
     <div style={{ color: `teal` }}>
       <Header headerText="About Gatsby" />
       <Header headerText="It's pretty cool" />
       <p>Such wow. Very React.</p>
+      <div>
+      <h1>About me</h1>
+      <p>
+        I’m good enough, I’m smart enough, and gosh darn it, people like me!
+      </p>
     </div>
+    </div>
+    </Layout>
   )
 }
